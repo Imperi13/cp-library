0c045810b84187b52277aa3dbad6459949d0419b
Imperi13 <imperi.night@gmail.com>
2020-05-16 15:51:19 +0900

    fix

--- UnionFind.cpp
+++ UnionFind.cpp
@@ -8,12 +8,12 @@ class UnionFind{
   private:
   using size_type=std::size_t;
 
-  std::vector<size_type> par,size;
+  std::vector<size_type> par,sz;
   size_type group;
 
   public:
 
-  UnionFind(size_type n):par(n,0),size(n,1),group(n){
+  UnionFind(size_type n):par(n,0),sz(n,1),group(n){
     std::iota(par.begin(),par.end(),0);
   }
 
@@ -25,15 +25,15 @@ class UnionFind{
   bool unite(size_type a,size_type b){
     a=root(a);b=root(b);
     if(a==b)return false;
-    if(size[a]<size[b])std::swap(a,b);
-    size[a]+=size[b];
+    if(sz[a]<sz[b])std::swap(a,b);
+    sz[a]+=sz[b];
     par[b]=a;
     return true;
   }
 
   size_type size(){return par.size();}
   size_type groups(){return group;}
-  size_type group_size(size_type n){return size[root(n)];}
+  size_type group_size(size_type n){return sz[root(n)];}
 
   bool same(size_type a,size_type b){return root(a)==root(b);}
 };
