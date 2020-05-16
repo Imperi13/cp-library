96f274e3e45567dc239b0f8b103ccebc60a03a55
Imperi13 <imperi.night@gmail.com>
2020-05-16 15:43:09 +0900

    add UFtest

---
+++ UnionFind.test.cpp
@@ -0,0 +1,22 @@
+#define PROBLEM "https://judge.yosupo.jp/problem/unionfind"
+
+#include <iostream>
+#include "UnionFind.cpp"
+
+#define rep(i, a, b) for (long long i = (a); (i) < (b); (i)++)
+
+using ll=long long;
+
+int main(){
+  ll n,q;
+  std::cin>>n>>q;
+
+  UnionFind uni(n);
+
+  rep(i,0,q){
+    ll t,u,v;
+    std::cin>>t>>u>>v;
+    if(t==0)uni.unite(u,v);
+    else std::cout<<(uni.same(u,v)?1:0)<<"\n";
+  }
+}
