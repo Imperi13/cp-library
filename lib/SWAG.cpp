a15b3c4226b52cd45e3f4199522934ee09a099b0
Imperi13 <imperi.night@gmail.com>
2020-05-20 19:11:00 +0900

    add SWAG

---
+++ lib/SWAG.cpp
@@ -0,0 +1,90 @@
+#pragma once
+
+#include <stack>
+#include <cassert>
+
+template<typename Semigroup>
+class SWAG{
+  public:
+  using value_t=typename Semigroup::value_t;
+  using size_t=std::size_t;
+  private:
+
+  using P=std::pair<value_t,value_t>;
+  std::stack<P> front,back;
+
+  public:
+  SWAG(){}
+
+  size_t size(){return front.size()+back.size();}
+  bool empty(){return size()==0;}
+
+  value_t fold_all(){
+    assert(!empty());
+    if(front.empty())return back.top().second;
+    if(back.empty())return front.top().second;
+    return Semigroup::op(front.top().second,back.top().second);
+  }
+
+  void push_back(value_t val){
+    if(back.empty())back.emplace(val,val);
+    else{
+      P tmp=back.top();
+      back.emplace(val,Semigroup::op(tmp.second,val));
+    }
+  }
+
+  void push_front(value_t val){
+    if(front.empty())front.emplace(val,val);
+    else{
+      P tmp=back.top();
+      back.emplace(val,Semigroup::op(val,tmp.second));
+    }
+  }
+
+  void pop_back(){
+    assert(!empty());
+    if(!back.empty()){
+      back.pop();
+      return;
+    }
+    size_t n=front.size();
+    std::stack<value_t> tmp;
+    for(size_t i=0;i<n/2;i++){
+      tmp.emplace(front.top().first);
+      front.pop();
+    }
+    while(!front.empty()){
+      push_back(front.top().first);
+      front.pop();
+    }
+    while(!tmp.empty()){
+      push_front(tmp.top());
+      front.pop();
+    }
+    back.pop();
+  }
+
+  void pop_front(){
+    assert(!empty());
+    if(!front.empty()){
+      front.pop();
+      return;
+    }
+    size_t n=back.size();
+    std::stack<value_t> tmp;
+    for(std::size_t i=0;i<n/2;i++){
+      tmp.emplace(back.top().first);
+      back.pop();
+    }
+    while(!back.empty()){
+      push_front(back.top().first);
+      back.pop();
+    }
+    while(!tmp.empty()){
+      push_back(tmp.top());
+      tmp.pop();
+    }
+    front.pop();
+  }
+};
---
+++ test/SWAG.test.cpp
@@ -0,0 +1,48 @@
+#define PROBLEM "https://judge.yosupo.jp/problem/queue_operate_all_composite"
+
+#include <iostream>
+#include "../lib/SWAG.cpp"
+
+#define rep(i, a, b) for (long long i = (a); (i) < (b); (i)++)
+
+using ll=long long;
+
+constexpr long long MOD=998244353;
+
+struct Semigroup{
+  using value_t=std::pair<long long,long long>;
+  static constexpr value_t op(value_t a,value_t b){
+    return {a.first*b.first%MOD,(a.second*b.first%MOD + b.second)%MOD};
+  }
+};
+
+int main(){
+  std::cin.tie(nullptr);
+  std::ios::sync_with_stdio(false);
+
+  SWAG<Semigroup> swag;
+
+  ll q;
+  std::cin>>q;
+
+  rep(i,0,q){
+    ll mode;
+    std::cin>>mode;
+
+    if(mode==0){
+      ll a,b;
+      std::cin>>a>>b;
+      swag.push_back({a,b});
+    }else if(mode==1){
+      swag.pop_front();
+    }else{
+      ll x;
+      std::cin>>x;
+      if(swag.empty())std::cout<<x<<"\n";
+      else{
+        auto tmp=swag.fold_all();
+        std::cout<<(tmp.first*x%MOD+tmp.second)%MOD<<"\n";
+      }
+    }
+  }
+}
