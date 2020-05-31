2da73ac6d65b77117ea2e1f2b15127c6a866d9c1
Imperi <imperi.night@gmail.com>
2020-05-31 15:13:59 +0900

    update SparseTable

--- lib/SparseTable.cpp
+++ lib/SparseTable.cpp
@@ -5,7 +5,6 @@
 
 //Semigroup
 // type value_t 
-// static value_t none;
 // static (value_t ,value_t)->value_t op
 
 // a op a == a
@@ -16,18 +15,27 @@ class SparseTable{
   using value_t=typename Semigroup::value_t;
   using size_t=std::size_t;
   private:
+  using Opt=std::pair<value_t,bool>;
+  Opt id={value_t(),false};
+
   size_t n;
   std::vector<size_t> ln;
-  std::vector<std::vector<value_t>> table;
+  std::vector<std::vector<Opt>> table;
+
+  constexpr inline Opt op(Opt a,Opt b){
+    if(a.second&b.second)return Opt(Semigroup::op(a.first,b.first),true);
+    if(a.second|b.second)return a.second?a:b;
+    return id;
+  }
   public:
   SparseTable(const std::vector<value_t>& a):n(a.size()),ln(n+1,0){
     for(size_t i=1;i<n+1;i++)ln[i]=ln[i-1]+(i>=(1ull<<(ln[i-1]+1)));
-    table=std::vector<std::vector<value_t>>(ln[n]+1,std::vector<value_t>(n,Semigroup::none));
-    for(size_t i=0;i<n;i++)table[0][i]=a[i];
+    table=std::vector<std::vector<Opt>>(ln[n]+1,std::vector<Opt>(n,id));
+    for(size_t i=0;i<n;i++)table[0][i]=Opt(a[i],true);
     for(size_t j=1;j<ln[n]+1;j++){
       for(size_t i=0;i<n;i++){
-        if(i+(1ll<<j) > n)table[j][i]=Semigroup::none;
-        else table[j][i]=Semigroup::op(table[j-1][i],table[j-1][i+(1ll<<(j-1))]);
+        if(i+(1ll<<j) > n)table[j][i]=id;
+        else table[j][i]=op(table[j-1][i],table[j-1][i+(1ll<<(j-1))]);
       }
     }
   }
@@ -35,6 +43,6 @@ class SparseTable{
   value_t fold(size_t l,size_t r){
     assert(0<=l&&l<r&&r<=n);
     size_t m=r-l;
-    return Semigroup::op(table[ln[m]][l],table[ln[m]][r-(1ll<<ln[m])]);
+    return op(table[ln[m]][l],table[ln[m]][r-(1ll<<ln[m])]).first;
   }
 };
--- test/SparseTable.test.cpp
+++ test/SparseTable.test.cpp
@@ -9,33 +9,35 @@
 #include "../lib/SparseTable.cpp"
 
 struct Semigroup{
-  using value_t=long long;
-  static constexpr value_t none=2e9;
-  static constexpr value_t op(const value_t& a,const value_t& b){
-    return std::min(a,b);
+  using value_t = long long;
+  static constexpr value_t op(const value_t &a, const value_t &b){
+    return std::min(a, b);
   }
 };
 
-using ll=long long;
-using P=std::pair<ll,ll>;
+using ll = long long;
+using P = std::pair<ll, ll>;
 
-int main(){
+int main()
+{
   std::cin.tie(nullptr);
   std::ios::sync_with_stdio(false);
 
-  ll n,q;
-  std::cin>>n>>q;
+  ll n, q;
+  std::cin >> n >> q;
 
   std::vector<ll> a(n);
-  for(auto&& e:a)std::cin>>e;
+  for (auto &&e : a)
+    std::cin >> e;
 
   SparseTable<Semigroup> sp(a);
 
-  while(q--){
-    ll l,r;
-    std::cin>>l>>r;
-    std::cout<<sp.fold(l,r)<<"\n";
+  while (q--)
+  {
+    ll l, r;
+    std::cin >> l >> r;
+    std::cout << sp.fold(l, r) << "\n";
   }
 
-  return 0; 
+  return 0;
 }
