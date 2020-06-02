b2f6d7bb22955f1654f84c2ec28be9833a3d27b4
Imperi <imperi.night@gmail.com>
2020-06-02 09:16:06 +0900

    update SparseTable

--- lib/SparseTable.cpp
+++ lib/SparseTable.cpp
@@ -3,8 +3,6 @@
 #include <vector>
 #include <cassert>
 
-#include "./utility/Monoid.cpp"
-
 //Semigroup
 // type value_t 
 // static (value_t ,value_t)->value_t op
@@ -17,22 +15,20 @@ class SparseTable{
   using value_t=typename Semigroup::value_t;
   using size_t=std::size_t;
   private:
-  using Opt=OptionalMonoid<Semigroup>;
-  using opt_t=typename Opt::value_t;
 
   size_t n;
   std::vector<size_t> ln;
-  std::vector<std::vector<opt_t>> table;
+  std::vector<std::vector<value_t>> table;
 
   public:
   SparseTable(const std::vector<value_t>& a):n(a.size()),ln(n+1,0){
     for(size_t i=1;i<n+1;i++)ln[i]=ln[i-1]+(i>=(1ull<<(ln[i-1]+1)));
-    table=std::vector<std::vector<opt_t>>(ln[n]+1,std::vector<opt_t>(n,Opt::id));
-    for(size_t i=0;i<n;i++)table[0][i]={a[i],true};
+    table=std::vector<std::vector<value_t>>(ln[n]+1,std::vector<value_t>(n,value_t()));
+    for(size_t i=0;i<n;i++)table[0][i]=a[i];
     for(size_t j=1;j<ln[n]+1;j++){
       for(size_t i=0;i<n;i++){
-        if(i+(1ll<<j) > n)table[j][i]=Opt::id;
-        else table[j][i]=Opt::op(table[j-1][i],table[j-1][i+(1ll<<(j-1))]);
+        if(i+(1ll<<j) > n)table[j][i]=value_t();
+        else table[j][i]=Semigroup::op(table[j-1][i],table[j-1][i+(1ll<<(j-1))]);
       }
     }
   }
@@ -40,6 +36,6 @@ class SparseTable{
   value_t fold(size_t l,size_t r){
     assert(0<=l&&l<r&&r<=n);
     size_t m=r-l;
-    return Opt::op(table[ln[m]][l],table[ln[m]][r-(1ll<<ln[m])]).first;
+    return Semigroup::op(table[ln[m]][l],table[ln[m]][r-(1ll<<ln[m])]);
   }
 };
