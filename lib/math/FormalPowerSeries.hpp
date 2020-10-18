ed8bd82ffb709a768f46e346c130bed7a46f5acc
Imperi13 <imperi.night@gmail.com>
2020-10-19 00:53:24 +0900

    FPSの逆元を追加

--- lib/math/FormalPowerSeries.hpp
+++ lib/math/FormalPowerSeries.hpp
@@ -18,61 +18,77 @@ class FPSoperator {
 
   std::vector<mint> w;
 
-  FPS fft(const FPS& a, bool inv) {
-    assert(a.size() == len);
-    auto dfs = [&](auto f, const FPS& coef, u64 l) {
-      assert(coef.size() == l);
-      if (l == 1) return coef;
-      FPS odd(l / 2), even(l / 2);
-      for (std::size_t i = 0; i < l; i++) {
-        if (i % 2 == 0)
-          even[i / 2] = coef[i];
-        else
-          odd[i / 2] = coef[i];
-      }
-      u64 half = l / 2, haba = len / l;
-      even = f(f, even, half);
-      odd = f(f, odd, half);
-
-      FPS ret(l);
-      for (std::size_t i = 0; i < l; i++) {
-        ret[i] = even[i % half] +
-                 w[inv ? (len - haba * i) : haba * i] * odd[i % half];
-      }
-      return ret;
-    };
-
-    FPS ret = dfs(dfs, a, len);
-    mint len_inv = mint(1) / len;
-    if (inv)
-      for (std::size_t i = 0; i < len; i++) ret[i] *= len_inv;
+  FPS fft(const FPS& a,u64 l,bool inv){
+    if(l==1)return a;
+    u64 half = l/2,haba = 2*len/l;
+    FPS even(half),odd(half);
+    for(std::size_t i=0;i<l;i++){
+      if(i%2==0)even[i/2]=a[i];
+      else odd[i/2]=a[i];
+    }
+    even = fft(even,half,inv);
+    odd = fft(odd,half,inv);
+    FPS ret(l);
+    for(std::size_t i=0;i<l;i++){
+      ret[i] = even[i%half] + w[inv ? (2*len-haba * i)%(2*len) : haba*i] * odd[i%half];
+    }
     return ret;
   }
 
+  FPS div(FPS a,u64 x){
+    mint inv =mint(1)/x;
+    for(auto&& e:a)e*=inv;
+    return a;
+  }
+
+  FPS mul(FPS a,FPS b,u64 l){
+    assert(a.size() == l && b.size() == l);
+    a.resize(2*l);b.resize(2*l);
+    a = fft(a,2*l,false);b = fft(b,2*l,false);
+    for(std::size_t i=0;i<2*l;i++)a[i] *= b[i];
+    a = fft(a,2*l,true);
+    a = div(a,2*l);
+    a.resize(l);
+    return a;
+  }
+
  public:
-  FPSoperator(u64 len_) : len_real(len_), w(len_real * 4, 1) {
+  FPSoperator(u64 len_) : len_real(len_){
     len = 1;
-    while (len < 2*len_real-1) len <<= 1;
-    assert((MOD - 1) % len == 0);
-    u64 bit = (MOD - 1) / len;
+    while (len < len_real) len <<= 1;
+    w = FPS(2*len,1);
+    assert((MOD - 1) % (2*len) == 0);
+    u64 bit = (MOD - 1) / (2*len);
     mint a = PRI_ROOT;
     while (bit > 0) {
       if (bit & 1) w[1] *= a;
       a *= a;
       bit >>= 1;
     }
-    for (std::size_t i = 2; i < len; i++) w[i] = w[i - 1] * w[1];
+    for (std::size_t i = 2; i < 2*len; i++) w[i] = w[i - 1] * w[1];
   }
 
-  FPS mul(FPS a, FPS b) {
+  FPS mul(FPS a,FPS b){
     assert(a.size() <= len_real && b.size() <= len_real);
-    a.resize(len);
-    b.resize(len);
-    a = fft(a, false);
-    b = fft(b, false);
-    for (std::size_t i = 0; i < len; i++) a[i] *= b[i];
-    a = fft(a, true);
+    a.resize(len);b.resize(len);
+    a = mul(a,b,len);
     a.resize(len_real);
     return a;
+  }
+
+  FPS inv(FPS a){
+    assert(a.size() <= len_real);
+    assert(a[0]!=0);
+    a.resize(len);
+    u64 now = 1;
+    FPS ret(1,mint(1)/a[0]);
+    while(now < len){
+      now<<=1;
+      ret.resize(now);
+      auto tmp = mul(FPS(a.begin(),a.begin()+now) , mul(ret,ret,now) ,now);
+      for(std::size_t i=0;i<now;i++)ret[i] = ret[i] * 2 - tmp[i];
+    }
+    ret.resize(len_real);
+    return ret;
   }
 };
